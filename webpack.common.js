276dfdb8a4f5511f61dfd15fccf2a83a48733f0d
Marek Szymanski <szyman.marek@gmail.com>
2019-09-14 13:39:00 +0200

    Added PWA

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 
 module.exports = {
     entry: {
@@ -11,6 +12,19 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'Smartbian',
             template: 'index.html'
+        }),
+        new WebpackPwaManifest({
+            name: 'Smartbian - home automation',
+            short_name: 'Smartbian',
+            description: 'Create your home automation based on Raspberry Pi and control it from one place...',
+            background_color: '#ffffff',
+            crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+            icons: [
+                {
+                    src: path.resolve('public/smartbian512.png'),
+                    sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+                }
+            ]
         })
     ],
     output: {
@@ -29,11 +43,11 @@ module.exports = {
                 test: /\.css$/,
                 loader: 'css-loader',
                 options: {
-                  url: true,
+                    url: true,
                 },
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
+                test: /\.(png|svg|jpg|gif|ico)$/,
                 use: [
                     'file-loader'
                 ]
